d89bfb6e5adb13b5739a43c128bee906ac1e5363
EVA_Jesse <pango3001@gmail.com>
2018-02-22 14:07:17 -0600

    made prime_guess.py more efficient

--- prime_guess.py
+++ prime_guess.py
@@ -1,10 +1,12 @@
+import math
+
 prime_guess = input("Guess a number you think is prime: ")
 prime_guess = int(prime_guess)
 higher_number = int(prime_guess) + 1
 
 for x in range(2, higher_number):
     prime = True
-    for y in range(2, x):
+    for y in range(2, int(math.sqrt(x)) + 1):
         if x % y == 0:
             prime = False
     if prime:
