a11e9aebe76ec473ce8523ef047202324b48e1fd
EVA_Jesse <pango3001@gmail.com>
2018-02-22 12:51:54 -0600

    added file prime_guess.py

---
+++ prime_guess.py
@@ -0,0 +1,18 @@
+prime_guess = input("Guess a number you think is prime: ")
+prime_guess = int(prime_guess)
+higher_number = int(prime_guess) + 1
+
+for x in range(2, higher_number):
+    prime = True
+    for y in range(2, x):
+        if x % y == 0:
+            prime = False
+            y += 1
+    if prime:
+        if x == prime_guess:
+            print("Correct", prime_guess, "is a prime number!")
+            break
+        else:
+            x += 1
+    elif x == (higher_number - 1):
+        print("Sorry", prime_guess, "is not a prime number")
